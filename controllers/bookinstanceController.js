9a881cd24f3cf5b62bbb015e96b6378d822f3876
quan-p <m.phamquan@gmail.com>
2022-05-09 17:55:45 -0500

    Render book instance form

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,4 +1,6 @@
 var BookInstance = require('../models/bookinstance');
+const { body,validationResult } = require('express-validator');
+var Book = require('../models/book');
 
 // Display list of all BookInstances.
 exports.bookinstance_list = function(req, res, next) {
@@ -33,9 +35,17 @@ exports.bookinstance_detail = function(req, res, next) {
 };
 
 // Display BookInstance create form on GET.
-exports.bookinstance_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance create GET');
+exports.bookinstance_create_get = function(req, res, next) {
+
+    Book.find({},'title')
+    .exec(function (err, books) {
+      if (err) { return next(err); }
+      // Successful, so render.
+      res.render('bookinstance_form', {title: 'Create BookInstance', book_list: books});
+    });
+
 };
+
 
 // Handle BookInstance create on POST.
 exports.bookinstance_create_post = function(req, res) {
